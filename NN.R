54feb7428966b52250a7d47787c92540b5453a24
Fatemeh Behjati <fbehjati@srv-13-246.mpi-inf.mpg.de>
2018-12-21 10:34:46 +0100

    imporved the NN models by swtiching from relu to tanh and added the plots representing the function learnt by the model

--- NN.R
+++ NN.R
@@ -7,6 +7,7 @@ library(rpgm)
 library(dHSIC)
 library(ggplot2)
 library(ggthemes)
+library(pheatmap)
 source("simulation.R")
 source("../DPM/R/nldata.R")
 
@@ -17,14 +18,18 @@ dir.create(file.path("/MMCI/MS/ExpRegulation/work/nonLinearCor/", output.path), 
 
 buildNN <- function(input_shape, activation= c("relu", "tanh", "selu", "sigmoid", "linear"), hidden.nodes= c(5, 5)){
   model <- keras_model_sequential()
+  l2_factor <- 0.00
+  l1_factor <- 0.00
 
-  model %>% layer_dense(units = hidden.nodes[1], activation = activation, input_shape = input_shape)
+  #model %>% layer_dense(units = hidden.nodes[1], activation = activation, input_shape = input_shape, kernel_regularizer= regularizer_l1_l2(l1 = 0.1, l2 = 0.1))
+  model %>% layer_dense(units = hidden.nodes[1], activation = activation, input_shape = input_shape) #, kernel_regularizer= regularizer_l1_l2(l1= l1_factor, l2= l2_factor))
   for(i in seq(2, length(hidden.nodes))){
-    model %>% layer_dense(units = hidden.nodes[i], activation = activation)
+    model %>% layer_dense(units = hidden.nodes[i], activation = activation) #, kernel_regularizer= regularizer_l1_l2(l1= l1_factor, l2= l2_factor))
   }
 
   model %>% layer_dense(units = 1)
 
+  #model %>% compile(loss= "mean_squared_error", optimizer = optimizer_adam(lr= .1), metrics= "mse")
   model %>% compile(loss= "mean_squared_error", optimizer = optimizer_adam(), metrics= "mse")
   #model %>% compile(loss= "mean_squared_error", optimizer = optimizer_rmsprop(), metrics= "mse")
 
@@ -36,12 +41,16 @@ normalizeMinMax <- function(train_X, train_Y1, train_Y2, test_X, test_Y1, test_Y
 {
     colMin <- colMins(train_X)
     colMax <- colMaxs(train_X)
+    colMin_Y1 <- min(train_Y1)
+    colMax_Y1 <- max(train_Y1)
+    colMin_Y2 <- min(train_Y2)
+    colMax_Y2 <- max(train_Y2)
     train_X <- scale(train_X, center=colMin, scale=(colMax - colMin))
     test_X <- scale(test_X, center=colMin, scale=(colMax - colMin))
-    train_Y1 <- scale(train_Y1, center=min(train_Y1), scale=(max(train_Y1) - min(train_Y1)))
-    test_Y1 <- scale(test_Y1, center=min(train_Y1), scale=(max(train_Y1) - min(train_Y1)))
-    train_Y2 <- scale(train_Y2, center=min(train_Y2), scale=(max(train_Y2) - min(train_Y2)))
-    test_Y2 <- scale(test_Y2, center=min(train_Y2), scale=(max(train_Y2) - min(train_Y2)))
+    train_Y1 <- scale(train_Y1, center= colMin_Y1, scale=(colMax_Y1 - colMin_Y1))
+    test_Y1 <- scale(test_Y1, center= colMin_Y1, scale=(colMax_Y1 - colMin_Y1))
+    train_Y2 <- scale(train_Y2, center= colMin_Y2, scale=(colMax_Y2 - colMin_Y2))
+    test_Y2 <- scale(test_Y2, center= colMin_Y2, scale=(colMax_Y2 - colMin_Y2))
     return(list(train_X, train_Y1, train_Y2, test_X, test_Y1, test_Y2))
 }
 
@@ -81,8 +90,10 @@ for (id1 in 1:(ncol(data)-1))
 {
 	for (id2 in (id1+1):ncol(data))
 	{
+#for(id1 in 1){
+#  for(id2 in 6){
         ## split data into test and training, and feature and responses
-        train.cnt <- 1000
+        train.cnt <- 800
         shuffle.idx <- sample(nrow(data))[seq(train.cnt)]
         train_X <- data[shuffle.idx, -c(id1,id2)]
         train_Y1 <- data[shuffle.idx, id1]
@@ -97,8 +108,8 @@ for (id1 in 1:(ncol(data)-1))
         #
         #train_Y <- scale(train_Y, center= T, scale= T)
         #test_Y <- scale(test_Y, center= mean(train_Y), scale= sd(train_Y))
-        #normalized_data <- normalizeMinMax(train_X, train_Y1, train_Y2, test_X, test_Y1, test_Y2)
-        normalized_data <- normalizeMinMax(train_X, train_Y1, train_Y2, test_X, train_Y1, train_Y2)
+        normalized_data <- normalizeMinMax(train_X, train_Y1, train_Y2, test_X, test_Y1, test_Y2)
+        #normalized_data <- normalizeMinMax(train_X, train_Y1, train_Y2, test_X, train_Y1, train_Y2)
         train_X <- normalized_data[[1]]
         train_Y1 <- normalized_data[[2]]
         train_Y2 <- normalized_data[[3]]
@@ -113,8 +124,8 @@ for (id1 in 1:(ncol(data)-1))
 
         #model.relu <- buildNN(activation= "relu", hidden.nodes= c(10, 5))
         #model.lin <- buildNN(activation= "linear", hidden.nodes= c(10, 5))
-        model.sigm1 <- buildNN(dim(train_X)[2], activation= "relu", hidden.nodes= c(20, 15, 10))
-        model.sigm2 <- buildNN(dim(train_X)[2], activation= "relu", hidden.nodes= c(20, 15, 10))
+        model.sigm1 <- buildNN(dim(train_X)[2], activation= "tanh", hidden.nodes= c(20, 15, 10))
+        model.sigm2 <- buildNN(dim(train_X)[2], activation= "tanh", hidden.nodes= c(20, 15, 10))
 
         #history.relu <- model.relu %>% fit(train_X, train_Y, epochs = 200, batch_size = 256, validation_split = 0.2)
         #history.lin <- model.lin %>% fit(train_X, train_Y, epochs = 200, batch_size = 256, validation_split = 0.2)
@@ -123,6 +134,9 @@ for (id1 in 1:(ncol(data)-1))
         history.sigm1 <- model.sigm1 %>% fit(train_X, train_Y1, epochs = 800, batch_size = 32, validation_split = 0.2, callbacks = list(early_stop, print_dot_callback))
         history.sigm2 <- model.sigm2 %>% fit(train_X, train_Y2, epochs = 800, batch_size = 32, validation_split = 0.2, callbacks = list(early_stop, print_dot_callback))
 
+
+        sigm1.weights <- get_weights(model.sigm1)
+        sigm2.weights <- get_weights(model.sigm2)
 
         #model.relu %>% evaluate(test_X, test_Y)
         #model.lin %>% evaluate(test_X, test_Y)
@@ -140,10 +154,10 @@ for (id1 in 1:(ncol(data)-1))
         #dev.off()
 
         ## compute residuals
-        pred1 <- model.sigm1 %>% predict(train_X)
-        residual1 <- train_Y1 - pred1
-        pred2 <- model.sigm2 %>% predict(train_X)
-        residual2 <- train_Y2 - pred2
+        pred1 <- model.sigm1 %>% predict(test_X)
+        residual1 <- test_Y1 - pred1
+        pred2 <- model.sigm2 %>% predict(test_X)
+        residual2 <- test_Y2 - pred2
         statT <- computeKernelTest(residual1, residual2)
         print(statT$p.value)
         res[[length(res)+1]] <- list(id1=id1, id2=id2, HSIT=statT)
@@ -153,16 +167,23 @@ for (id1 in 1:(ncol(data)-1))
         pdf(paste(output.path, "/NN_sigm",colnames(data)[id1],"_comb",colnames(data)[id1],"+",colnames(data)[id2],".pdf", sep= ""))
         summary(model.sigm1)
         print(plot(history.sigm1))
-        print(ggplot(data.frame(id1 = train_Y1, id2 = train_Y2), aes(x=id1, y=id2)) + geom_point() + coord_equal() + theme_tufte())
+        print(ggplot(data.frame(id1 = test_Y1, id2 = test_Y2), aes(x=id1, y=id2)) + geom_point() + coord_equal() + theme_tufte())
         print(ggplot(data.frame(res1 = residual1, res2 = residual2), aes(x=res1, y=res2)) + geom_point() + coord_equal() + theme_tufte())
-        print(ggplot(data.frame(id1 = train_Y1, pred = pred1), aes(x=id1, y=pred)) + geom_point() + coord_equal() + theme_tufte())
+        print(ggplot(data.frame(id1 = test_Y1, pred = pred1), aes(x=id1, y=pred)) + geom_point() + coord_equal() + theme_tufte())
+        print(ggplot(data.frame(x = test_Y2, y= test_Y1, pred = pred1), aes(x=x)) + geom_point(aes(y=y)) + geom_point(aes(y=pred), color="yellow") + coord_equal() + theme_tufte())
+
+        for(i in seq(1, length(sigm1.weights), by= 2))
+          pheatmap(rbind(sigm1.weights[[i]], sigm1.weights[[(i + 1)]]), cluster_cols= F, cluster_rows= F, main= paste("layer", i, "to", (i + 1)))
         dev.off()
         pdf(paste(output.path, "/NN_sigm",colnames(data)[id2],"_comb",colnames(data)[id1],"+",colnames(data)[id2], ".pdf", sep= ""))
         summary(model.sigm2)
         print(plot(history.sigm2))
-        print(ggplot(data.frame(id1 = train_Y1, id2 = train_Y2), aes(x=id1, y=id2)) + geom_point() + coord_equal() + theme_tufte())
+        print(ggplot(data.frame(id1 = test_Y1, id2 = test_Y2), aes(x=id1, y=id2)) + geom_point() + coord_equal() + theme_tufte())
         print(ggplot(data.frame(res1 = residual1, res2 = residual2), aes(x=res1, y=res2)) + geom_point() + coord_equal() + theme_tufte())
-        print(ggplot(data.frame(id2 = train_Y2, pred = pred2), aes(x=id2, y=pred)) + geom_point() + coord_equal() + theme_tufte())
+        print(ggplot(data.frame(id2 = test_Y2, pred = pred2), aes(x=id2, y=pred)) + geom_point() + coord_equal() + theme_tufte())
+        print(ggplot(data.frame(x = test_Y1, y= test_Y2, pred = pred2), aes(x=x)) + geom_point(aes(y=y)) + geom_point(aes(y=pred), color="yellow") + coord_equal() + theme_tufte())
+        for(i in seq(1, length(sigm2.weights), by= 2))
+          pheatmap(rbind(sigm2.weights[[i]], sigm2.weights[[(i + 1)]]), cluster_cols= F, cluster_rows= F, main= paste("layer", i, "to", (i + 1)))
         dev.off()
         
 	}
